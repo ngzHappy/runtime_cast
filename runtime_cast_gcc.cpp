1f5b90302fe6ec82037abade68bff37b477c891a
ngzHappy <zhaixueqiang@hotmail.com>
2018-12-01 08:12:03 +0800

    +change namespace

--- runtime_cast_gcc.cpp
+++ runtime_cast_gcc.cpp
@@ -6,7 +6,7 @@ void * runtime_dynamic_cast(
         void * argInput/*dynamic_cast<void *>*/,
         const std::type_info * argInputType/*type_id(remove_cvr)*/,
         const std::type_info * argOutputType/*type_id(remove_cvr)*/) {
-    using namespace __cxxabiv1 ;
+    using namespace abi ;
     return __dynamic_cast(argInput,
                           static_cast<const __class_type_info*>(argInputType),
                           static_cast<const __class_type_info*>(argOutputType),-1);
